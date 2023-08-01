95bc9844966ddb52e9abb27c8cc599b661c92780
FabrizioPC <fabrizioxz1@gmail.com>
2023-07-31 20:24:29 -0500

    Change title quotebox

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,12 +3,12 @@ import "./styles/QuoteBox.css";
 const QuoteBox = ({ handleChangeQuote, quote }) => {
    return (
       <section className="quoteBox">
-         <h1>Fortune Cookies</h1>
+         <h1>Galletas de la fortuna</h1>
          <section>
-            <button onClick={handleChangeQuote}>Try luck</button>
             <article>
                <p>{quote.phrase}</p>
             </article>
+            <button onClick={handleChangeQuote}>Try luck</button>
          </section>
          <footer>
             <h4>Fuente: {quote.author}</h4>
